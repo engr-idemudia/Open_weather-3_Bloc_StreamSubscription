e8674f0e8090e2f1fb7e015cb1d2e7749ddd8adc
Idemudia <idemudiamondayosaghae@gmail.com>
2023-02-20 07:52:43 +0200

    import bloc and make chages in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:open_weather_cubit/cubits/theme/theme_cubit.dart';
-import 'package:open_weather_cubit/cubits/weather/weather_cubit.dart';
-import 'package:open_weather_cubit/repositories/weather_repository.dart';
-import 'package:open_weather_cubit/services/weather_api_services.dart';
 
-import 'cubits/temp_settings/temp_settings_cubit.dart';
+import 'repositories/weather_repository.dart';
+import 'services/weather_api_services.dart';
+
 import 'pages/home_page.dart';
+
+import 'blocs/blocs.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -28,21 +28,21 @@ class MyApp extends StatelessWidget {
       ),
       child: MultiBlocProvider(
         providers: [
-          BlocProvider<WeatherCubit>(
-            create: (context) => WeatherCubit(
+          BlocProvider<WeatherBloc>(
+            create: (context) => WeatherBloc(
               weatherRepository: context.read<WeatherRepository>(),
             ),
           ),
-          BlocProvider<TempSettingsCubit>(
-            create: (context) => TempSettingsCubit(),
+          BlocProvider<TempSettingsBloc>(
+            create: (context) => TempSettingsBloc(),
           ),
-          BlocProvider<ThemeCubit>(
-            create: (context) => ThemeCubit(
-              weatherCubit: context.read<WeatherCubit>(),
+          BlocProvider<ThemeBloc>(
+            create: (context) => ThemeBloc(
+              weatherBloc: context.read<WeatherBloc>(),
             ),
           ),
         ],
-        child: BlocBuilder<ThemeCubit, ThemeState>(
+        child: BlocBuilder<ThemeBloc, ThemeState>(
           builder: (context, state) {
             return MaterialApp(
               title: 'Weather App',
